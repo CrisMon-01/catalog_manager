5c99f64b5e497621619f74ad81f53f34ce34a3ce
Cristian Monti <cristian.monti@sourcesense.com>
2018-08-23 12:29:47 +0200

    modifica per nuovo server, gestione config file

--- kubernetes/config-map-test.sh
+++ kubernetes/config-map-test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/***REMOVED*** bash
-kubectl --kubeconfig=../../.kube/config.***REMOVED***-***REMOVED*** delete configmap catalog-manager-conf
-kubectl --kubeconfig=../../.kube/config.***REMOVED***-***REMOVED*** create configmap catalog-manager-conf --from-file=../conf/test/prodBase.conf
+kubectl --kubeconfig=../../../.kube/config.***REMOVED***-***REMOVED*** delete configmap catalog-manager-conf
+kubectl --kubeconfig=../../../.kube/config.***REMOVED***-***REMOVED*** create configmap catalog-manager-conf --from-file=../conf/test/prodBase.conf
